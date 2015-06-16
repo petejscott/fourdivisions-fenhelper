ecaa2487f44d2ad49d85417b3d70387ef249da17
petejscott <pete.j.scott@gmail.com>
2015-06-15 20:25:50 -0400

    Add tests for urlLib.getUrlParams()

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,31 @@
 'use strict';
 
-QUnit.test( "framework test", function (assert)
+QUnit.test( "urlLib.getUrlParams - when URL contains ?foo=bar, urlParams['foo'] returns 'bar'", function (assert)
 {
-	assert.ok( 1 === 1 );
+	var sut = {
+		search : '?foo=bar'
+	};
+	var urlParams = urlLib.getUrlParams(sut);
+	
+	assert.ok( urlParams["foo"] === "bar" );
+});
+
+QUnit.test( "urlLib.getUrlParams - when URL contains ?foo=bar&bar=baz, urlParams['foo'] returns 'bar'", function (assert)
+{
+	var sut = {
+		search : '?foo=bar&bar=baz'
+	};
+	var urlParams = urlLib.getUrlParams(sut);
+	
+	assert.ok( urlParams["foo"] === "bar" );
+});
+
+QUnit.test( "urlLib.getUrlParams - when URL contains ?foo=bar&bar=baz, urlParams['bar'] returns 'baz'", function (assert)
+{
+	var sut = {
+		search : '?foo=bar&bar=baz'
+	};
+	var urlParams = urlLib.getUrlParams(sut);
+	
+	assert.ok( urlParams["bar"] === "baz" );
 });
