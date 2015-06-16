8aa888127419355decfa3646250ee6f117daf9c4
petejscott <pete.j.scott@gmail.com>
2015-06-16 08:39:21 -0400

    Add a few more urlLib tests

--- tests/urlLib_tests.js
+++ tests/urlLib_tests.js
@@ -29,3 +29,23 @@ QUnit.test( "urlLib.getUrlParams - when URL contains ?foo=bar&bar=baz, urlParams
 	
 	assert.ok( urlParams["bar"] === "baz" );
 });
+
+QUnit.test( "urlLib.getUrlParams - when URL contains ?foo=bar&bar=baz, urlParams['x'] returns undefined", function (assert)
+{
+	var sut = {
+		search : '?foo=bar&bar=baz'
+	};
+	var urlParams = urlLib.getUrlParams(sut);
+	
+	assert.ok( typeof(urlParams["x"]) === "undefined" );
+});
+
+QUnit.test( "urlLib.getUrlParams - when URL contains ?foo=bar&bar=baz&empty, urlParams['empty'] returns empty string", function (assert)
+{
+	var sut = {
+		search : '?foo=bar&bar=baz&empty'
+	};
+	var urlParams = urlLib.getUrlParams(sut);
+	
+	assert.ok( urlParams["empty"] === "" );
+});
